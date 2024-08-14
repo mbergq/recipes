75ccd98268472ed16152c3b496d1a5e315313d11
Martin <106612709+menkodot@users.noreply.github.com>
2024-08-14 13:51:47 +0200

    add simple react-navigation

--- App.js
+++ App.js
@@ -1,21 +1,40 @@
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Button } from "react-native";
 import Test from "./Test";
 
-export default function App() {
+function HomeScreen({ navigation }) {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Recipes"
+        onPress={() => navigation.navigate("Recipes")}
+      />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Recipes" component={Test} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#fff",
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+// });
--- Test.tsx
+++ Test.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import { Text } from "react-native";
 function Test() {
-  return <Text>Hej</Text>;
+  return <Text>Recipes</Text>;
 }
 
 export default Test;
