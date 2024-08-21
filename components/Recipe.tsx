aab660eddd72b514d1e7ec8e5dc25a83fc7f1491
Martin <106612709+menkodot@users.noreply.github.com>
2024-08-21 10:43:12 +0200

    add index on child items for focus mode

--- components/Recipe.tsx
+++ components/Recipe.tsx
@@ -137,15 +137,19 @@ function Recipe({ route }) {
               <Text>Focus mode</Text>
               <StyledDescription>Ingredienser</StyledDescription>
               {recipe.ingredients &&
-                recipe.ingredients.map((i) => (
-                  <Text>{i.name + "- " + i.amount}</Text>
+                recipe.ingredients.map((ingredient, index) => (
+                  <Text key={index}>
+                    {ingredient.name + "- " + ingredient.amount}
+                  </Text>
                 ))}
             </StyledIngView>
             <StyledInsView>
               <StyledDescription>Instruktioner</StyledDescription>
               {recipe.instructions &&
-                recipe.instructions.map((instruction) => (
-                  <StyledText>{instrNumber++ + ". " + instruction}</StyledText>
+                recipe.instructions.map((instruction, index) => (
+                  <StyledText key={index}>
+                    {instrNumber++ + ". " + instruction}
+                  </StyledText>
                 ))}
             </StyledInsView>
           </RecipeLayout>
