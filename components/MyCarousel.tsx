233b4f85f7f527c92072b00d9b9b2cddb4b74da9
Martin <106612709+menkodot@users.noreply.github.com>
2024-08-21 13:38:35 +0200

    add example carousel, change app to tsx

--- components/MyCarousel.tsx
+++ components/MyCarousel.tsx
@@ -1,8 +1,47 @@
-import { Text } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 import Carousel from "pinar";
 
+const styles = StyleSheet.create({
+  slide1: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#a3c9a8",
+  },
+  slide2: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#84b59f",
+  },
+  slide3: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#69a297",
+  },
+  text: {
+    color: "#1f2d3d",
+    opacity: 0.7,
+    fontSize: 48,
+    fontWeight: "bold",
+  },
+});
+
 function MyCarousel() {
-  return <Text>Carousel</Text>;
+  return (
+    <Carousel>
+      <View style={styles.slide1}>
+        <Text style={styles.text}>1</Text>
+      </View>
+      <View style={styles.slide2}>
+        <Text style={styles.text}>2</Text>
+      </View>
+      <View style={styles.slide3}>
+        <Text style={styles.text}>3</Text>
+      </View>
+    </Carousel>
+  );
 }
 
 export default MyCarousel;
